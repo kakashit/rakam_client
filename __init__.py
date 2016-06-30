974f649f771e1d181bd15352e24d52f045e2d74a
Kaan Uğurlu <kugurlu94@gmail.com>
2016-06-30 20:54:17 +0300

    initial

---
+++ __init__.py
@@ -0,0 +1,15 @@
+"""
+    Rakam client for limited use.
+    Project is only made for use in multiple internal projects, hence it only
+    contains a small amount of functionality.
+"""
+
+
+__version__ = '0.1.0'
+__author__ = 'Kaan Uğurlu'
+__contact__ = 'kugurlu94@gmail.com'
+__all__ = ['RakamClient', 'RakamCredentials']
+
+
+from rakam.connection import RakamCredentials
+from rakam.client import RakamClient
